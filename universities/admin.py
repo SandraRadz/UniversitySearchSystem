452a522cc706f39bec1689c0ee2657f35f3183e7
SandraRadz <oleksandraradzievska@gmail.com>
2020-11-24 22:56:44 +0200

    add universities models to admin page

--- universities/admin.py
+++ universities/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from universities.models import Country, StudyProgram, University
+
+admin.site.register(Country)
+admin.site.register(StudyProgram)
+admin.site.register(University)
