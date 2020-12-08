5170159196d9b5122b89a9f02b57e66d43ddff32
SandraRadz <oleksandraradzievska@gmail.com>
2020-12-08 21:18:54 +0200

    fix url order

--- universities/urls.py
+++ universities/urls.py
@@ -7,9 +7,9 @@ from universities.views import UniversityDetailView, UniversityListView, study_p
 urlpatterns = [
     path('', UniversityListView.as_view(), name='university-list'),
     path('api/v1/universities/', UniversityList.as_view()),
-    path('<slug:slug>/', UniversityDetailView.as_view(), name='university-detail'),
     path('api/v1/filter/', study_program_filter, name='study_program_filter'),
     path('study-programs/', UniversityStudyProgramListView.as_view(), name='study-program-list'),
     path('study-program/<slug:slug>/', UniversityStudyProgramDetailView.as_view(), name='study-program-detail'),
+    path('<slug:slug>/', UniversityDetailView.as_view(), name='university-detail'),
 
 ]
